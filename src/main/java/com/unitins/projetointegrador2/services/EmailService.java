8b2e7efdced28ddd19d958f2c4880a11068f802c
david-botelho-mariano <48680041+david-botelho-mariano@users.noreply.github.com>
2020-04-26 20:51:51 -0300

    v0.1

--- src/main/java/com/unitins/projetointegrador2/services/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.unitins.projetointegrador2.services;
-
-import org.springframework.mail.SimpleMailMessage;
-
-public interface EmailService {
-    void sendEmail(SimpleMailMessage email);
-}
