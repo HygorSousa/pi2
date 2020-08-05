1709c91f0167069d604e50adf4b46c58d14b2bb6
david-botelho-mariano <48680041+david-botelho-mariano@users.noreply.github.com>
2020-08-05 20:20:53 -0300

    v15

---
+++ src/main/java/com/unitins/projetointegrador2/model/Curso.java
@@ -0,0 +1,20 @@
+package com.unitins.projetointegrador2.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String descricao;
+    private String nome;
+
+    @OneToMany
+    private List<Professor> professores;
+}
